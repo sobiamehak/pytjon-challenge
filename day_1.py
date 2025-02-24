2907b96facecacb8809deb6eb10deb690b6d84a0
Sobia mehak <161197811+sobiamehak@users.noreply.github.com>
2025-02-24 09:42:24 +0500

    Add files via upload
    This is my first python code Hello python

---
+++ day_1.py
@@ -0,0 +1,14 @@
+# #🐍 Day 1 - Python Challenge: Print Your First Message!
+
+# 📢 Challenge:
+# Apna pehla Python program likho jo screen par ye message print kare:
+
+# "Hello, Python Learners! 🚀 Today is the first step in our coding journey."
+
+# 🔹 Bonus Challenge:
+# Agar tum apna naam bhi add karna chahte ho, to program ko modify karo taake wo tumhara naam bhi print kare!
+
+
+day1 = "Hello, Python Learners! 🚀"
+my_name = "Sobia Mehak." 
+print(f"{day1} My name is {my_name} Today is the first step in our coding journey.")
